e9c6bd1347da0dd1df9000608762c9145eaf82b3
luong0104 <havanluong0104@gmail.com>
2023-07-31 15:28:14 +0700

    helo

---
+++ flutter_application_2/lib/screens/details/details_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/screens/details/components/body.dart';
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
